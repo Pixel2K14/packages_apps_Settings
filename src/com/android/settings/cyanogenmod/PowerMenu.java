NamelessRom/android_packages_apps_Settings

[2/2] Settings: On-The-Go: Hide on devices without camera
Hide camera switching for devices without front camera as well
if we dont have a front camera, open the first camera we have.
also hide the options from the dialog.

Change-Id: Ic74579708dc4ff030cca97f1f5972dce105431d8

src/com/android/settings/cyanogenmod/PowerMenu.java
@@ -22,6 +22,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 
+import com.android.internal.util.nameless.NamelessUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -51,5 +52,10 @@ public class PowerMenu extends SettingsPreferenceFragment {
             getPreferenceScreen().removePreference(
                     findPreference(Settings.System.POWER_MENU_USER_ENABLED));
         }
+
+        findPreference(Settings.Nameless.POWER_MENU_ONTHEGO_ENABLED).setEnabled(
+                NamelessUtils.hasCamera(getActivity())
+        );
+
     }
 }

Pixel2K14/packages_apps_Settings

src/com/android/settings/cyanogenmod/PowerMenu.java
@@ -22,6 +22,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 
+import com.android.internal.util.nameless.NamelessUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -46,5 +47,10 @@ public class PowerMenu extends SettingsPreferenceFragment {
             getPreferenceScreen().removePreference(
                     findPreference(Settings.System.POWER_MENU_USER_ENABLED));
         }
+
+        findPreference(Settings.System.POWER_MENU_ONTHEGO_ENABLED).setEnabled(
+                NamelessUtils.hasCamera(getActivity())
+        );
+
     }
 }